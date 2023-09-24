ca41f4cbf59823238fa0435a82b381c0df39c691
Murat Miftakhov <murmif02gmail.com>
2023-09-24 21:36:27 +0300

    added search

--- src/app/views.py
+++ src/app/views.py
@@ -6,7 +6,12 @@ def home_page(request):
     return render(request, "home.html")
 
 def services_list_page(request):
-    return render(request, "service_select.html", {"specialists": data.specialists})
+    query = request.GET.get("q")
+    if query:
+        specialists = [s for s in data.specialists if query.lower() in s['name'].lower()]
+    else:
+        specialists = data.specialists
+    return render(request, "service_select.html", {"specialists": specialists, "query": query})
 
 def service_page(request, id):
     return render(request, "service_info.html", {"specialist": data.specialists[id]})
